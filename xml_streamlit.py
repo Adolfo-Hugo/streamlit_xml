b0827a98cea5fe17e2a16b4324adc60191699c51
Adolfo-Hugo <adolfohugosilvadev@gmail.com>
2024-10-26 01:14:50 -0300

    Add files via upload

--- xml_streamlit.py
+++ xml_streamlit.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
 
 # Função para download de XML com barra de progresso atualizada em tempo real
 def download_xml(manual_keys, download_path):
